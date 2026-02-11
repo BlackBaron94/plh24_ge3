d55bce3469b7129eb04b133718874221bb0f8bbd
Panagiotis Sofianopoulos <sofianopoulospanagiotis@gmail.com>
2026-02-11 15:26:52 +0200

    cleanup

--- eap_wiki/src/main/java/com/plh24/packageAPI/WikiApiClient.java
+++ eap_wiki/src/main/java/com/plh24/packageAPI/WikiApiClient.java
@@ -12,8 +12,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
-import org.json.JSONArray;
-import org.json.JSONObject;
 public class WikiApiClient {
     private final OkHttpClient client = new OkHttpClient();
     
