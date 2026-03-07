0910c7d7fa7dea3b43a9b0e012c9b16ad7943c19
apistel-st <apistelas@gmail.com>
2026-03-07 08:40:03 +0200

    σχόλια JavaDoc

--- eap_wiki/src/main/java/com/plh24/packageEntities/Article.java
+++ eap_wiki/src/main/java/com/plh24/packageEntities/Article.java
@@ -21,8 +21,9 @@ import java.time.LocalDateTime;
 
 
 /**
- * Η κλάση Article δείχνει ένα άρθρο στη ΒΔ
+ * Η κλάση Article δείχνει ένα άρθρο στη Βάση Δεδομένων
  * Σύνδεση με Category ManyToOne.
+ * @author Αντώνιος Πιστέλας
  */
 
 
@@ -35,35 +36,70 @@ import java.time.LocalDateTime;
 
 public class Article {
 
-    
-    //Πεδία
-    
+
+    /**
+     *
+     * Primary Key
+     * Δημιουργείται αυτόματα στη Βάση Δεδομένων.
+     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ARTICLE_ID")//Στη DB η στήλη λέγεται ARTICLE_ID
     private Long article_id;  //primary key auto generated
 
+    /**
+     * Τίτλος άρθρου.
+     * μοναδικός,δεν μπορεί να είναι null.
+     */
     @Column(nullable = false, unique = true)
     private String title; //Δε μπορεί να είναι null
+
+
+    /**
+     * Βαθμολογια άρθρου.
+     * Μπορεί να είναι null.
+     */
 
     @Column(nullable = true)
     private Integer rating;
 
+
+    /**
+     * Ημερομηνία και ώρα αποθηκευσης του άρθρου
+     */
     @Column(name = "saved_at", nullable = false)
     private LocalDateTime savedAt; //Ημερομηνία δημιουργίας
 
+    /**
+     * Κατηγορία σπου ανήκει το άρθρο.
+     * Σχέση ManyToOne με Category
+     */
     @ManyToOne(optional = false)
     @JoinColumn(name = "category_id")
     private Category category; //Πρέπει να υπάρχει κατηγορία
 
+
+    /**
+     * Σχόλια για το άρθρο
+     */
     @Column(name = "comments", nullable = true)
     private String comments;
     // Κατασκευαστες
-    
-    
+
+
     public Article() { //default constractor για JPA
     }
-    
+
+    /**
+     * Constructor δημιουργίας άρθρου με βαθμολογία.
+     *
+     *
+     * @param title τίτλος άρθρου
+     * @param rating βαθμολογία άρθρου
+     * @param category κατηγορία άρθρου
+     * @param comments σχόλια άρθρου
+     */
     public Article(String title, int rating, Category category, String comments) {
         this.title = title;
         this.rating = rating;
@@ -71,6 +107,16 @@ public class Article {
         this.savedAt = LocalDateTime.now();//η τρέχουσα ώρα και ημερομηνία
         this.comments = comments;
     }
+
+      /**
+     * Constructor δημιουργίας άρθρου χωρίς βαθμολογία.
+     *
+     *
+     * @param title τίτλος άρθρου
+     * @param category κατηγορία άρθρου
+     * @param comments σχόλια άρθρου
+     */
+
     public Article(String title, Category category, String comments) {
         this.title = title;
         this.rating = null;
@@ -78,58 +124,114 @@ public class Article {
         this.savedAt = LocalDateTime.now();
         this.comments = comments; // μπορεί να είναι null
     }
-   
+
     // Getters
+
+
+    /**
+     * @return μοναδικό ID άρθρου
+     */
     public Long getId() {
         return article_id;
     }
 
+
+     /**
+     * @return title τίτλο άρθρου.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @return rating βαθμολογία άρθρου
+     */
     public Integer getRating(){
         return rating;
     }
 
+     /**
+     * @return savedAt ημερομηνία αποθήκευσης του άθρου.
+     */
     public LocalDateTime getSavedAt() {
         return savedAt;
     }
 
+
+    /**
+     * @return category κατηγορία του άρθρου
+     */
     public Category getCategory() {
         return category;
     }
 
+
+    /**
+     *
+     * @return category όνομα κατηγορίας, κενό string αν δεν υπάρχει
+     */
     public String getCategoryName() {
         return category != null ? category.getName() : "";
     }
-    
+
+    /**
+     * @return comments σχόλια άρθρου
+     */
     public String getComments() {
         return comments;
     }
 
-    
-    
+
+
     // Setters
+
+
+    /**
+     * βαζει βαθμολογία στο άρθρο
+     * @param rating  βαθμολογία του άθρου
+     */
     public void setRating(Integer rating) {
         this.rating = rating;
 }
 
+
+    /**
+     *
+     * @param category νέα κατηγορία
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
-    
+
+
+    /**
+
+     * @param comments νέα σχόλια
+     */
     public void setComments(String comments) {
         this.comments = comments;
     }
 
-  
+
     // Equals, hashCode, toString
+
+
+
+    /**
+     * @return hash code υπολοσγισμός hash code βάση το ID του άρθρου.
+     */
+
     @Override
     public int hashCode() {
         return (article_id != null ? article_id.hashCode() : 0);
     }
 
+
+    /**
+     * Ελέγχει αν δύο άρθρα είναι ίσα βάση του ID.
+     * @param object αντικείμενο προς σύγκριση
+     * @return true αν είναι το ίδιο άρθρο
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Article)) return false;
@@ -137,6 +239,10 @@ public class Article {
         return (this.article_id != null || other.article_id == null) && (this.article_id == null || this.article_id.equals(other.article_id));
     }
 
+
+ /**
+     * @return τίτλος άρθρου ως String
+     */
     @Override
     public String toString() {
         return title;
--- eap_wiki/src/main/java/com/plh24/packageEntities/Category.java
+++ eap_wiki/src/main/java/com/plh24/packageEntities/Category.java
@@ -19,14 +19,37 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+
+/**
+ * Η κλάση Category αναπαριστά μια κατηγορία άρθρων
+ * στη Βάση Δεδομένων.
+ * Πολλά άρθρα μπορούν να ανήκουν στην ίδια κατηγορία ManyToOne 
+ *
+ * @author Αντώνιος Πιστέλας
+ */
+
 @Entity//η κλάση είναι JPA Entity και θα αντιστοιχηθεί σε πίνακα DB
 
 
 @Table(name = "category")//Ονομα του πίνακας στη MySQL είναι "category"
 
 @NamedQueries({
+    
+    /**
+     * Επιστρoφή όλων των κατηγορίων από τη Βάση Δεδομένων.
+     */
     @NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c"),
+    
+    
+    /**
+     * Αναζητηση κατηγορίας με βάση το ID της.
+     */
     @NamedQuery(name = "Category.findByCategoryId", query = "SELECT c FROM Category c WHERE c.categoryId = :categoryId"),
+    
+    
+    /**
+     * Αναζήτηση κατηγορίας με βάση το όνομα.
+     */
     @NamedQuery(name = "Category.findByName", query = "SELECT c FROM Category c WHERE c.name = :name")
 })
 
@@ -35,13 +58,19 @@ public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Primary Key
+     * Δημιουργείται αυτόματα από τη Βάση Δεδομένων.
+     */
     @Id//Primary key του πίνακα category
     @GeneratedValue(strategy = GenerationType.IDENTITY)//auto-increment
-    
-    
     @Column(name = "CATEGORY_ID")//Στη DB η στήλη λέγεται CATEGORY_ID
     private Integer categoryId;
 
+     /**
+     * 
+     * Μοναδικό όνομα κατηγορίας το οποίο δε πρέπει να είναι null.
+     */
     @Column(name = "CATEGORy_NAME", nullable = false, unique = true)//Όνομα κατηγορίας: not null και unique
     private String name;
 
@@ -49,11 +78,22 @@ public class Category implements Serializable {
     
     
     // Κατασκευαστες
-    public Category() {
-        // Empty, Default constructor for JPA
+    
+    
+    /**
+     * Προεπιλεγμένος constructor
+     * Απαιτείται από το JPA framework 
+     */
+    
+    public Category() { //default constractor για JPA
     }
 
-    // Δημιουργία νέας κατηγορίας με όνομα (για DB insert)
+    
+    /** 
+     * Constructor δημιουργία νέας κατηγορίας 
+     * 
+     * @param name όνομα της κατηγορίας 
+     */
     public Category(String name) {
         this.name = name;
     }
@@ -62,10 +102,19 @@ public class Category implements Serializable {
     // Getters & Setters
     
 
+    
+    /**
+     * Επιστροφή του μοναδικό ID της κατηγορίας.
+     * @return categoryId
+     */
     public Integer getCategoryId() {
         return categoryId;
     }
 
+    /**
+     * Επιστρέφει όνομα της κατηγορίας.
+     * @return name κατηγορίας
+     */
     public String getName() {
         return name;
     }
@@ -73,11 +122,22 @@ public class Category implements Serializable {
     
     // Equals, HashCode & ToString
 
+    
+    /**
+     * Υπολογίζει hash code με βάση το ID της κατηγορίας.
+     * @return hash code
+     */
     @Override
     public int hashCode() {
         return (categoryId != null ? categoryId.hashCode() : 0);
     }
 
+    
+     /**
+     * Ελέγχει με βάση το categoryId αν δύο αντικείμενα Category είναι ίσα
+     * @param object αντικείμενο προς σύγκριση
+     * @return true αν τα αντικείμενα είναι ίσα
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Category)) return false;
@@ -86,6 +146,11 @@ public class Category implements Serializable {
                 && (this.categoryId == null || this.categoryId.equals(other.categoryId));
     }
 
+    
+    /**
+     * Επιστρέφει το όνομα της κατηγορίας ως String.
+     * @return name κατηγορίας
+     */
     @Override
     public String toString() {
         return name; // Εμφανίζεται στο JComboBox
